e737fc935f58aa7a446ab09e2e17e09432651bfe
DaviAieta <aieta.davi@gmail.com>
2025-03-04 15:09:33 -0700

    learning params and post

--- first-app/src/tasks/tasks.controller.ts
+++ first-app/src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('/tasks')
@@ -6,12 +6,19 @@ export class TasksController {
   constructor(private readonly taskService: TasksService) {}
 
   @Get()
-  getTasks() {
-    return this.taskService.listAllTasks();
+  getTasks(@Query('limit') limit: string) {
+    console.log(limit);
+    return this.taskService.listAll();
   }
 
-  @Get('/2')
-  getTask() {
-    return this.taskService.findOneTask();
+  @Get(':id')
+  getTask(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Post('/create')
+  createTask(@Body() body: string) {
+    console.log(body);
+    return this.taskService.create(body);
   }
 }
--- first-app/src/tasks/tasks.service.ts
+++ first-app/src/tasks/tasks.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
-  listAllTasks() {
+  listAll() {
     return [
       { id: 1, task: 'Go to walmart' },
       { id: 2, task: 'Make dinner' },
     ];
   }
 
-  findOneTask() {
-    return 'Make dinner';
+  findOne(id: string) {
+    return 'Searching for tasks with ID: ' + id;
+  }
+
+  create(body: string) {
+    return body;
   }
 }
